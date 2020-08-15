93d9b6651a19cb333059f8188be57922ace2754f
Laura Torres <ltorresfdz@gmail.com>
2020-08-14 23:54:49 -0500

    PyBank listo, PyPoll no se liegan los datos

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -8,10 +8,20 @@ name = []
 nombre_candidato =[]
 votes={}
 porc_primero=0
+porc_pri = 0
 porc_segundo=0
+porc_seg=0
 porc_tercero=0
+porc_ter=0
 porc_cuarto=0
-para_imprimir =""
+porc_cuar=0
+primero =0
+segundo =0
+tercero =0
+cuarto =0
+nombres=[]
+names=[]
+votitos=[]
 
 csvpath = os.path.join("..","Resources","Homework3_PyPoll_Resources_election_data.csv")
 # Open and read csv
@@ -35,50 +45,61 @@ def winner(input):
      # create another dictionary and it's key will 
      # be count of votes values will be name of  
      # candidates 
-     dict = {} 
+     candidate = {} 
   
      for value in votes.values(): 
   
-          # initialize empty list to each key to  
-          # insert candidate names having same  
+          # initialize empty list to each key to insert candidate names having same  
           # number of votes  
-          dict[value] = [] 
+          candidate[value] = [] 
   
      for (key,value) in votes.items(): 
-          dict[value].append(key)
-          
-  
-     # sort keys in descending order to get maximum  
-     # value of votes 
-     primero = sorted(dict.keys(),reverse=True)[0] 
+          candidate[value].append(key)
+       # sort keys in descending order to get maximum  value of votes 
+     primero = sorted(candidate.keys(),reverse=True)[0] 
      porc_primero = (primero / total_votes)*100
-     segundo = sorted(dict.keys(),reverse=True)[1] 
+     segundo = sorted(candidate.keys(),reverse=True)[1] 
      porc_segundo = (segundo/total_votes)*100
-     tercero = sorted(dict.keys(),reverse=True)[2]
+     tercero = sorted(candidate.keys(),reverse=True)[2]
      porc_tercero =(tercero / total_votes)*100
-     cuarto = sorted(dict.keys(),reverse=True)[3]
+     cuarto = sorted(candidate.keys(),reverse=True)[3]
      porc_cuarto = (cuarto/total_votes)*100  
-     print(primero)
-     print(f'{dict[primero]}')
-     print(porc_primero)
-     print(segundo)
-     print(f'{dict[segundo]}')
-     print(porc_segundo)
-     print(tercero)
-     print(f'{dict[tercero]}')
-     print(porc_tercero)
-     print(cuarto)
-     print(porc_cuarto)
-     print(f'{dict[cuarto]}')
 
-winner(nombre_candidato) 
+
+        
+     print(f'{candidate[primero]}')
+     print("{0:.3f}".format(porc_primero)) 
+     porc_pri = "{0:.3f}".format(porc_primero)
+     print(primero)
+     print(f'{candidate[segundo]}')
+     print("{0:.3f}".format(porc_segundo))
+     porc_seg ="{0:.3f}".format(porc_segundo)
+     print(segundo)
+     print(f'{candidate[tercero]}')
+     print("{0:.3f}".format(porc_tercero))
+     porc_ter ="{0:.3f}".format(porc_tercero)
+     print(tercero)
+     print(f'{candidate[cuarto]}')
+     print("{0:.3f}".format(porc_cuarto))
+     porc_cuar ="{0:.3f}".format(porc_cuarto)
+     print(cuarto)
+
+winner(nombre_candidato)  
+
+ 
+
 
 print("  	Election Results")
 print("---------------------------------------")
 print(f"Total Votes:  {str(total_votes)}")
 print("---------------------------------------")
-print(str(dict[primero]) + ": " + str(porc_primero) + "% " + "(" + str(primero) + ")")
+print(f"Khan: {str(porc_primero)}  %  ( {str(primero)})")
+print(f"Correy: {str(porc_segundo)}  %  ( {str(segundo)})")
+print(f"Li: {str(porc_tercero)}  %  ( {str(tercero)})")
+print(f"O'Tooley: {str(porc_cuarto)}  %  ( {str(cuarto)})")
+
+#print(f"Greatest Increase in Profits: {str(fechas[Posicion_mayor])}  (${str(Maximo_cambio)})")
 
 
 
-#print ('%.2f'%a) 
+
